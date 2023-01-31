696a3718202de13dc28934f3c811b81237c1e3c4
SSAFY <anska77@naver.com>
2023-01-31 13:28:27 +0900

    Update: front-login

---
+++ Front/frontend/src/api/userApi.js
@@ -0,0 +1,38 @@
+/*
+ User API 예시
+ */
+ import { apiInstance } from "./index";
+
+ const api = apiInstance();
+ 
+ function requestLogin(data, callback, errorCallback) {
+     api.post(`/user/login`, JSON.stringify(data))
+     .then(callback)
+     .catch(errorCallback)
+ }
+ 
+ function requestJoin(data, callback, errorCallback) {
+     api.post(`/user/join`, JSON.stringify(data))
+     .then(callback)
+     .catch(errorCallback)
+ }
+ 
+//  function requestUpdate(data, callback, errorCallback) {
+//      api.post(`/account/update, JSON.stringify(data)`)
+//      .then(callback)
+//      .catch(errorCallback)
+//  }
+ 
+ // const requestLogin = (data,callback,errorCallback) => {
+ //     //백앤드와 로그인 통신하는 부분
+ //     callback();
+ 
+ // }
+ 
+ // const UserApi = {
+ //     requestLogin:(data,callback,errorCallback)=>requestLogin(data,callback,errorCallback)
+ // }
+ 
+ // export default UserApi
+ 
+ export {requestJoin, requestLogin};
---
+++ Front/frontend/src/views/icebreaking/IceMainView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapA">
+    <h1>Ice Breaking</h1>
+    <div class="wrap-btn">
+      <button class="btn-ice" @click="goMakeRoomIce">ICE1</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import router from "@/router";
+
+export default {
+  methods: {
+    goMakeRoomIce() {
+      router.push({ name: "MakeRoomIce" });
+    }, // 로그인 여부에 따라 보내야함 
+  }
+};
+</script>
+
+<style>
+</style>
