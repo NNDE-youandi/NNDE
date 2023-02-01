28793373a3ad46477d3a4b1be5b97b69c20e5030
kimsongsub <busanbak2000@naver.com>
2023-02-02 00:36:51 +0900

    Update indext.js

--- Front/frontend/src/router/index.js
+++ Front/frontend/src/router/index.js
@@ -1,83 +1,83 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/intro/HomeView.vue'
-import LoginView from '../views/user/LoginView.vue'
-import SignupView from '../views/user/SignupView.vue'
-import SelectModeView from '../views/intro/SelectModeView.vue'
-import GameMainView from '../views/game/GameMainView.vue'
-import MakeRoomIceView from '../views/intro/MakeRoomIceView.vue'
-import IceQrView from '../views/intro/IceQrView.vue'
-import SurveyView from '../views/intro/SurveyView.vue'
-import WaitingRoomView from '../views/intro/WaitingRoomView.vue'
-import IceBreakingStartView from '../views/icebreaking/IceBreakingStartView.vue'
-import Step1CountView from '../views/icebreaking/Step1CountView.vue'
-import KeyWordView from '../views/icebreaking/KeyWordView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/intro/HomeView.vue";
+import LoginView from "../views/user/LoginView.vue";
+import SignupView from "../views/user/SignupView.vue";
+import SelectModeView from "../views/intro/SelectModeView.vue";
+import GameMainView from "../views/game/GameMainView.vue";
+import MakeRoomIceView from "../views/intro/MakeRoomIceView.vue";
+import IceQrView from "../views/intro/IceQrView.vue";
+import SurveyView from "../views/intro/SurveyView.vue";
+import WaitingRoomView from "../views/intro/WaitingRoomView.vue";
+import IceBreakingStartView from "../views/icebreaking/IceBreakingStartView.vue";
+import Step1CountView from "../views/icebreaking/Step1CountView.vue";
+import KeyWordView from "../views/icebreaking/KeyWordView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: HomeView
+    path: "/",
+    name: "Home",
+    component: HomeView,
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: LoginView
+    path: "/login",
+    name: "Login",
+    component: LoginView,
   },
   {
-    path: '/signup',
-    name: 'Signup',
-    component: SignupView
+    path: "/signup",
+    name: "Signup",
+    component: SignupView,
   },
   {
-    path: '/selectmode',
-    name: 'SelectMode',
-    component: SelectModeView
+    path: "/selectmode",
+    name: "SelectMode",
+    component: SelectModeView,
   },
   {
-    path: '/gamemain',
-    name: 'GameMain',
-    component: GameMainView
+    path: "/gamemain",
+    name: "GameMain",
+    component: GameMainView,
   },
   {
-    path: '/makeroomice',
-    name: 'MakeRoomIce',
-    component: MakeRoomIceView
+    path: "/makeroomice",
+    name: "MakeRoomIce",
+    component: MakeRoomIceView,
   },
   {
-    path: '/iceqr',
-    name: 'IceQr',
-    component: IceQrView
+    path: "/iceqr",
+    name: "IceQr",
+    component: IceQrView,
   },
   {
-    path: '/survey',
-    name: 'Survey',
-    component: SurveyView
+    path: "/survey",
+    name: "Survey",
+    component: SurveyView,
   },
   {
-    path: '/watingroom',
-    name: 'WaitingRoom',
-    component: WaitingRoomView
+    path: "/waitingroom",
+    name: "WaitingRoom",
+    component: WaitingRoomView,
   },
   {
-    path: '/icebreakingstart',
-    name: 'IceBreakingStart',
-    component: IceBreakingStartView
+    path: "/icebreakingstart",
+    name: "IceBreakingStart",
+    component: IceBreakingStartView,
   },
   {
-    path: '/step1count',
-    name: 'Step1Count',
-    component: Step1CountView
+    path: "/step1count",
+    name: "Step1Count",
+    component: Step1CountView,
   },
   {
-    path: '/keyword',
-    name: 'KeyWord',
-    component: KeyWordView
+    path: "/keyword",
+    name: "KeyWord",
+    component: KeyWordView,
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
