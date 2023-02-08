7ca59a3313da200f9e67927d1aaa91a57dfcc042
seahare-slug <70680118+seahare-slug@users.noreply.github.com>
2023-02-08 16:58:42 +0900

    Update App.vue

--- Front/frontend/src/App.vue
+++ Front/frontend/src/App.vue
@@ -1,24 +1,18 @@
 <template>
   <div id="app">
     <!-- <HeaderView /> -->
-    <div>나는 로그인 상태야? {{ isLogin }}</div>
     <router-view> </router-view>
   </div>
 </template>
 
 <script>
 import "./components/css/style.scss";
-import { useStore } from "vuex";
-import { ref } from "vue";
 
 // import HeaderView from "./components/common/HeaderView.vue";
 export default {
-  setup() {
-    const store = useStore();
-    const isLogin = ref(store.state.userStore.isLogin);
-    return {
-      isLogin,
-    };
+  name: "App",
+  components: {
+    // HeaderView,
   },
 };
 </script>
