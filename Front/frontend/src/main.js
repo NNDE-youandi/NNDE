dfd6d399a5a801b3788390566eb3c05dd81c6349
SSAFY <anska77@naver.com>
2023-02-13 14:45:51 +0900

    Update: main.js

--- Front/frontend/src/main.js
+++ Front/frontend/src/main.js
@@ -5,8 +5,9 @@ import store from './store'
 import io from 'socket.io-client';
 // AWS 서버 IP
 // const socket = io('http://i8c209.p.ssafy.io:3001', {
+  // const socket = io("http://localhost:3001")
 // 로컬 서버 IP
-const socket = io('/', {
+const socket = io('http://localhost:3001', {
   // 폴링 대신 웹소켓만 사용, 폴링 사용시 CORS 에러 발생
   transports: ["websocket"]
 });
